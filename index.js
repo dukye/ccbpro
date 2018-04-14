a798c052ec20c646e9cf23bf07619dddabf10b07
dooke <me@duke.fr>
2018-04-14 02:13:11 +0200

    Parsing & TV

--- index.js
+++ index.js
@@ -12,26 +12,24 @@ http.createServer(function (req, res) {
 }).listen(process.env.PORT || 8080);
 console.log('HTTP Server running & listening...');
 
-
 /**
  * Rich embed discord
  * @type {Discord}
  */
-const makeEmbed = (title, desc, elements = []) => {
+const makeEmbed = (title, message, elements = []) => {
   const embed = new Discord.RichEmbed()
     .setTitle(`${title}`)
-    // .setAuthor('CryptoBot', 'http://www.scpc.org.au/wp-content/uploads/2016/10/rocketkid-1024x682.jpg')
+    // .setAuthor('CryptoBot', 'http://kedu.fr/image.png')
     .setColor(0x4671ed)
-    .setDescription(desc)
+    .setDescription(message)
     .setTimestamp()
     .setFooter('-- to the moon');
 
-    elements.forEach((obj, i) => {
-      embed.addField(obj.title, obj.value, true)
-      if ((i+=1) < elements.length) {
-        // embed.addBlankField(true);
-      }
-    });
+    if (elements.length > 0) {
+      elements.forEach((obj, i) => {
+        embed.addField(obj.title, obj.value, true)
+      });
+    }
   return embed;
 };
 
@@ -41,13 +39,30 @@ bot.on('ready', () => {
 
 bot.on('message', message => {
   const user = message.author;
+  const msg = message.content;
 
   if (message.content === 'ping') {
     message.reply('pong');
   }
-  const msg = message.content;
 
-  console.log(`--> Request from ${user.username}`);
+  console.log(`--> Request from ${user.username} in ${message.channel.name}`);
+  if (user.username == 'duke') {
+    const regCommand = new RegExp(/https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/ig);
+    const matchcommands = msg.match(regCommand);
+
+    // There is an url
+    if (matchcommands.length > 0) {      
+      var matches = matchcommands[0].match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+      var domain = matches && matches[1].replace('www.', '');
+      
+      // Tradingview ?
+      if (domain === 'tradingview.com') {
+        // message.channel.send(msg);
+        message.guild.channels.find('name', 'test').sendMessage(msg);
+      }
+    }
+
+  }
 });
 
 bot.login(process.env.DISCORDBOT);
